93f4b4a4da9f36aae7cd2ee4f979f17421b88e55
Igor Khotin <khotin@gmx.com>
2020-04-20 23:21:53 +0300

    [+] gameover sfx

--- trap/gameover.js
+++ trap/gameover.js
@@ -1,3 +1,17 @@
 function gameover() {
     env.state = 'gameover'
+
+    function turnOff() {
+        sfx.play('deviceOff', 1)
+    }
+
+    turnOff()
+    setTimeout(turnOff, 600)
+    setTimeout(turnOff, 800)
+    setTimeout(turnOff, 1000)
+    /*
+    for (let i = 0; i < 10; i++) {
+        setTimeout(turnOff, 500 + RND(5000))
+    }
+    */
 }
