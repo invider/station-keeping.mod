b382e2ef0b28f767222d30ce80ad4a20ff81079b
Igor Khotin <khotin@gmx.com>
2020-04-26 23:09:23 +0300

    [+] open and empty lockers on gameover

--- trap/gameover.js
+++ trap/gameover.js
@@ -2,6 +2,19 @@ function gameover() {
     env.state = 'gameover'
     lab.control.player.releaseAll()
 
+    lab.cam._ls.forEach(e => {
+        if (e instanceof dna.Locker) {
+            e.inUse = true
+            for (let i = 0; i < e.capacity; i++) {
+                e.items[i] = {
+                    type: 'broken'
+                }
+            }
+        } else if (e instanceof dna.TradeControl) {
+            e.qty = 0
+        }
+    })
+
     sfx.play('powerDown', 1)
 
     setTimeout(() => lib.tfx.roll(env.credits), 5000)
